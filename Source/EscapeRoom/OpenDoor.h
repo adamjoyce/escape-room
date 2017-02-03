8eedd733461eb4a6d47947a1c8e69bdff946cdd1
Adam Joyce <adamjoyce@outlook.com>
2017-02-03 15:25:53 +0000

    Added C++ component to open the door

---
+++ Source/EscapeRoom/OpenDoor.h
@@ -0,0 +1,26 @@
+// Copyright Adam Joyce 2017
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPEROOM_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
--- Source/EscapeRoom/PositionReporter.cpp
+++ Source/EscapeRoom/PositionReporter.cpp
@@ -10,7 +10,7 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -29,10 +29,10 @@ void UPositionReporter::BeginPlay()
 
 
 // Called every frame.
-void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
-{
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
-
-	// ...
-}
+//void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+//{
+//	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+//
+//	// ...
+//}
 
