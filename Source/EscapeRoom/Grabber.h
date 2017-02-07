fd4bb31d2622e45dbd4219821da2e84686da3d4f
Adam Joyce <adamjoyce@outlook.com>
2017-02-07 18:02:14 +0000

    Setup customised GameMode and Pawn

---
+++ Source/EscapeRoom/Grabber.h
@@ -0,0 +1,23 @@
+// Copyright Adam Joyce 2017.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPEROOM_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+};
