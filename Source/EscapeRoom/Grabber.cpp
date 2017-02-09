701def299312f2e8017533e1717a4751cea0dfc9
Adam Joyce <adamjoyce@outlook.com>
2017-02-09 18:40:11 +0000

    Raycasting for grabber

--- Source/EscapeRoom/Grabber.cpp
+++ Source/EscapeRoom/Grabber.cpp
@@ -28,16 +28,30 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// Get the player's viewpoint.
+	/// Get the player's viewpoint.
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	PlayerController->GetPlayerViewPoint(PlayerViewPointLocation, PlayerViewPointRotation);
 
-	// Logs the player controller's current viewpoint.
+	/// Logs the player controller's current viewpoint.
 	//UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
 
-	// Draw out grab line.
+	/// Draw out grab line.
 	const FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
 	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor::Red, false, 0.0f, 0.0f, 2.0f);
+
+	/// Setup collisiom query parameters.
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	/// Perform the raycast.
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParameters);
+
+	/// Log any object hit by the raycast.
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), *(ActorHit->GetName()));
+	}
 }
 
